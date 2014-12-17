ae553b896a3df1f02ae9b39052330f7e51979513
Kazuma Muramatsu <muramatu.k@gmail.com>
2014-12-17 13:56:34 +0900

    Add initialize

--- createdb.sh
+++ createdb.sh
@@ -9,3 +9,7 @@ set -e
 # create database
 
   ${CREATEDB} -h ${HOST} -U ${USER} -p ${PORT} ${DB}
+
+# initialize pgbench
+
+  ${PGBENCH} -i -s ${SCALE_FACTOR} -F ${FILLFACTOR} -h ${HOST} -U ${USER} -p ${PORT} ${DB}
